0b92cc3842996ee657b2b43aa1f96a5ad49bdfc4
Carson Schulz <schulzcarson@gmail.com>
2024-09-16 10:22:36 +0200

    Quickfix

--- wp-content/themes/shiba-plugins/inc/customizer.php
+++ wp-content/themes/shiba-plugins/inc/customizer.php
@@ -56,6 +56,6 @@ function shiba_plugins_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function shiba_plugins_customize_preview_js() {
-	wp_enqueue_script( 'shiba-plugins-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'shiba-plugins-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), SHIBA_PLUGINS_VERSION, true );
 }
 add_action( 'customize_preview_init', 'shiba_plugins_customize_preview_js' );
